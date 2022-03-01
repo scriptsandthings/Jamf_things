00254e5e2a6609c80cbc774fb37764733d459afa
Greg Knackstedt <52809959+gknackstedt@users.noreply.github.com>
2022-03-01 15:16:32 -0500

    Create Jamf_Kill_Running_Process.sh

---
+++ Bash/Jamf_Kill_Running_Process.sh
@@ -0,0 +1,46 @@
+#!/bin/zsh
+#
+# Jamf_Kill_Running_Process.sh
+# v1.0
+# 3.1.2022
+# Greg Knackstedt
+# shitttyscripts@gmail.com
+#
+###########################################
+# Script to check if a process is running, 
+# and if found send the "killall" command 
+# until the process is no longer running.
+# Uses Jamf script param 4 to define the process name.
+#############################################
+# define the target process
+killMe="$4"
+#
+echo "#########################"
+echo "#########################"
+echo "Checking if the process $killMe is running"
+echo "#########################"
+echo "#########################"
+#
+# Check if the process is running
+if pgrep $killMe; then
+    echo "#########################"
+    echo "Found process $killMe running on system..."
+	echo "#########################"
+    # While the process is running, do a loop to shut it down
+    while pgrep $killMe;
+    	do
+        	echo "#########################"
+			echo "#########################"
+			echo "Closing $killMe...."
+			echo "#########################"
+            # send kill command to process name
+   			killall "$killMe"
+  			sleep 1
+		done
+
+fi
+echo "#########################"
+echo "#########################"
+echo "The process $killMe is not running."
+echo "#########################"
+echo "#########################"
