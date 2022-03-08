8e7fe030ae4987f19e7e22d5fb0ae84852b1de64
Greg Knackstedt <52809959+gknackstedt@users.noreply.github.com>
2022-03-07 21:49:08 -0500

    Create Asset Management - Approximate City or Locality.sh

---
+++ Extension Attributes/Asset Management - Approximate City or Locality.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Gets the system's approximate city or state it's currently located in by using its current external facing IP
+#
+# v1.0
+# 3.7.2022
+#
+# Greg Knackstedt
+# https://github.com/gknackstedt
+# Shitttyscripts@gmail.com
+#
+####################################
+#
+# Get the current external IP address
+myIP=$(curl -L -s --max-time 10 http://checkip.dyndns.org | egrep -o -m 1 '([[:digit:]]{1,3}.){3}[[:digit:]]{1,3}')
+# Use the IP address to identify the current City
+City=$(curl -L -s --max-time 10 "http://ip-api.com/line/$myIP?fields=city")
+#
+# Display the results in a format that can be used as an extension attribute
+echo "<result>$City</result>"
