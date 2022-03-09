a462c341ec42e716e13995cc73aebd52518ebc85
Greg Knackstedt <52809959+gknackstedt@users.noreply.github.com>
2022-03-08 19:39:20 -0500

    Update Display_Network_Info_With_Self_Service.sh

--- Bash/Display_Network_Info_With_Self_Service.sh
+++ Bash/Display_Network_Info_With_Self_Service.sh
@@ -7,8 +7,9 @@ ScriptName="Display_Network_Info_With_Self_Service"
 # Previous versions of macOS do not include the "networkQuality" binary, which will cause the script to spit back an error.
 #
 #
-# This script displays an AppleScript dialog showing the IP addresses of the
-# default Ethernet and Airport interfaces (by just showing the first couple enX addresses), 
+# This script displays an AppleScript dialog showing the current date/time,
+# the version and build number of macOS installed on the system, the shortname of the currently logged in user,
+# the Mac's current IP addresses of the default Ethernet and Airport interfaces (by just showing the first couple enX addresses), 
 # as well as IP addresses for Cisco AnyConnect and Palo Alto GlobalProtect VPN.
 # Script also runs the "networkQuality" command to test upload/download speeds,
 # displaying the results via an AppleScript dialog.
