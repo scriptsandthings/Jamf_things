aad246751b0e20316439c6ca3e689ccc21a95eb2
Greg Knackstedt <52809959+gknackstedt@users.noreply.github.com>
2022-03-09 09:38:08 -0500

    Create ProofPoint_ObserveIT_Installed_Version.sh

---
+++ Extension Attributes/ProofPoint_ObserveIT_Installed_Version.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+########################################################################
+# A script to collect information on if ProofPoint Observe IT
+# is currently installed. Returns version number if installed,
+# If ObserveIT is not installed then "Not Installed" will return back
+#
+# v1.0
+# 3.9.2022
+# Greg Knackstedt
+# https://github.com/gknackstedt/
+# shitttyscripts@gmail.com
+#
+########################################################################
+if [ -e /Library/PEA/agent ]
+then 
+  fileversion_number="/Library/PEA/agent/version"
+	while read -r line; do
+    version_number="$line"
+     echo "<result>$version_number</result>"
+	done < "$fileversion_number"
+else
+  echo "<result>Not Installed</result>"
+fi
