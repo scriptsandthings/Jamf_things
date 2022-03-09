a48228ef663db3e35a4657a2de2bffa3c19f8492
Greg Knackstedt <52809959+gknackstedt@users.noreply.github.com>
2022-03-08 19:45:10 -0500

    Create Display_SMB_Connection_Stats.sh

---
+++ Bash/Display_SMB_Connection_Stats.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Display_SMB_Connection_Stats.sh
+#
+# Displays current smb mountpoint stats using AppleScript
+#
+# v1.0
+# 3.8.2022
+#
+# The output isn't beautiful or anything, but it's nice information to have.
+#
+# Greg Knackstedt
+# https://github.com/gknackstedt/
+# shitttyscripts@gmail.com
+#
+#
+ShowSMBStatus=$(smbutil statshares -a)
+Timestamp=$(date)
+
+/usr/bin/osascript << EOF
+
+tell application "Finder"
+
+activate
+
+display dialog "SMB share status for $USER at $Timestamp" & return & return & "NOTE: If formatting is difficult to read, copy/paste results into a text editor." & return & return & "$ShowSMBStatus" buttons {"================================================= Dismiss ================================================="} with icon caution
+
+end tell
+
+EOF
+
+
+exit 0
