ce52437243e186fff21bf0904f1e6d0587ca2abc
Greg Knackstedt <52809959+gknackstedt@users.noreply.github.com>
2022-03-08 19:40:26 -0500

    Update Display_IP_Addresses_With_Self_Service.sh

--- Bash/Display_IP_Addresses_With_Self_Service.sh
+++ Bash/Display_IP_Addresses_With_Self_Service.sh
@@ -2,8 +2,8 @@ ScriptName="Display_IP_Addresses_With_Self_Service"
 #
 #
 # Uses Apple Script to display a dialog with the current date/time, the version and build number of macOS installed on the system,
-# the Mac's current local WiFi and Ethernet IP addresses (by tossing out a couple enX results),
-# if connected the Mac's current VPN IP addresses (a few utunX results), and PaloAlto GlobalProtect VPN (some gpdX results).
+# the shortname of the currently logged in user, the Mac's current local WiFi and Ethernet IP addresses (by tossing out a couple enX results),
+# and if connected the Mac's current VPN IP addresses (a few utunX results), and PaloAlto GlobalProtect VPN (some gpdX results).
 #
 # The output isn't beautiful or anything, but it's nice information to have.
 #
