6d8c96142823137a2e9280ae3c8f5ffc10901f75
Scripts and Things <52809959+scriptsandthings@users.noreply.github.com>
2022-04-14 18:50:26 -0400

    Update Adobe_CC_ExManCMD_via_Jamf_Pro.sh

--- Bash/Adobe_CC_ExManCMD_via_Jamf_Pro.sh
+++ Bash/Adobe_CC_ExManCMD_via_Jamf_Pro.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
+#
+#############################
+#############################
+#
 # Greg Knackstedt
 # 5.13.2021
 #
-# Script to use ExManCMD to install ZXP plugins for Adobe CC
+# Script to use ExManCMD to install ZXP plugins for Adobe CC via Jamf Pro
+#
+#############################
+#############################
+#
 # Variables
+#
+# Full path to directory containing ZXP file to install
 ZXPDirectory="$4"
+# ZXP File name
 ZXPToInstall="$5"
+# Full path to directory containing ExManCMD
 exManDirectory="$6"
+# Full path to ExManCMD
 exManPath="$7"
+#
+#############################
+#############################
+#
+# Script
+#
 # Change permissions on pre-staged installation files
 chmod -Rf 777 "$ZXPDirectory"
 chmod -Rf 777 "$exManDirectory"
