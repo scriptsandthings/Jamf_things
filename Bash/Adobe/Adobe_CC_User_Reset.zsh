4e8c745be43a0435a87dcc7bd1cdff06417f696a
Scripts and Things <52809959+scriptsandthings@users.noreply.github.com>
2022-04-14 20:47:10 -0400

    Update and rename Adobe_CC_User_Reset.sh to Adobe_CC_User_Reset.zsh

--- Bash/Adobe/Adobe_CC_User_Reset.zsh
+++ Bash/Adobe/Adobe_CC_User_Reset.zsh
@@ -1,15 +1,31 @@
-#!/bin/sh
-
+#!/bin/zsh
+#
+##############################
+##############################
+#
+#  Before running this script, try the script User_Cache_And_Saved_App_State_Reset.zsh to see if it resolves the Adobe issue.
+#
+##############################
+##############################
+#
 #  Reset user level files for Adobe. Removes all Adobe files from /Users/$CurrentUser/Library,
 #  as well as all files from /Users/$CurrentUser/Library/Caches,
 #  and /Users/$CurrentUser/Library/Saved Application State/
 #  for the currently logged in user.
 #
-#  Created by Greg Knackstedt on 4.10.18.
-
+##############################
+##############################
+#
+#  Created by Greg Knackstedt
+#  4.10.18
+#  https://github.com/scriptsandthings/
+#
+##############################
+##############################
+#
 #find current user
 CurrentUser=$(/usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | /usr/bin/awk -F': ' '/[[:space:]]+Name[[:space:]]:/ { if ( $2 != "loginwindow" ) { print $2 }}')
-
+#
 #Remove everything Adobe and all caches from currently logged in user's /Library/
 rm -Rf /Users/$CurrentUser/Library/Application\ Support/Adob*
 rm -Rf /Users/$CurrentUser/Library/Caches/*
@@ -23,4 +39,4 @@ rm -Rf /Users/$CurrentUser/Library/LaunchAgents/com.adob*
 rm -Rf /Users/$CurrentUser/Library/Group\ Containers/Adob*
 rm -Rf /Users/$CurrentUser/Library/Group\ Containers/com.adob*
 
-exit
+exit 0
