ab5c9086053d6e688f4988fa3641d22cf1eb133a
Scripts and Things <52809959+scriptsandthings@users.noreply.github.com>
2022-04-14 22:47:32 -0400

    Update Nudge_Reset_Deferrals_And_Preferences.zsh

--- Bash/Nudge/Nudge_Reset_Deferrals_And_Preferences.zsh
+++ Bash/Nudge/Nudge_Reset_Deferrals_And_Preferences.zsh
@@ -15,5 +15,5 @@
 # Remove Nudge system wide preferences
 rm -Rfv /Library/Preferences/com.github.macadmins.Nudge.plist
 # Remove Nuge plist from all user directories
-rm -rfv /Users/*/Library/Preferences/com.github.macadmins.Nudge.plist
+rm -fv /Users/*/Library/Preferences/com.github.macadmins.Nudge.plist
 exit 0
