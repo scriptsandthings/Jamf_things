943d94e4f9eeeff7abe2357832d1bb6dce521dd8
Scripts and Things <52809959+scriptsandthings@users.noreply.github.com>
2022-05-12 10:36:21 -0400

    Update Fiery_Staged_Driver_Uninstall_v1.sh

--- Bash/Fiery/Fiery_Staged_Driver_Uninstall_v1.sh
+++ Bash/Fiery/Fiery_Staged_Driver_Uninstall_v1.sh
@@ -1,1 +1,22 @@
 
+#!/bin/bash
+#
+####################################
+#
+# Fiery_Staged_Driver_Uninstall_v1.sh
+#
+# Silently uninstalls Fiery Printer Drivers with help of .app staged in /tmp
+# v1.0
+# https://github.com/scriptsandthings
+#
+# Greg Knackstedt
+# 5.12.2022
+#
+
+#
+sudo /tmp/Fiery\ Printer\ Driver\ Installer.app/Contents/Resources/User\ Software/OSX/Printer\ Driver/Installer\ Wizard.app/Contents/MacOS/Fiery\ Printer\ Driver\ Installer remove
+#
+# Delete staged Fiery driver .app
+sudo rm -Rf /tmp/Fiery\ Printer\ Driver\ Installer.app
+
+exit 0
