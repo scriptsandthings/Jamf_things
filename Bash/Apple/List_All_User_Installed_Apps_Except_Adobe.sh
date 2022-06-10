afb9a33a3649f12955ed36c6443b9aae3c97ac80
Scripts and Things <ShitttyScripts@gmail.com>
2022-06-10 11:24:28 -0400

    Update List_All_User_Installed_Apps_Except_Adobe.sh

--- Bash/Apple/List_All_User_Installed_Apps_Except_Adobe.sh
+++ Bash/Apple/List_All_User_Installed_Apps_Except_Adobe.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 #######################
 #
-# List_All_User_Installed_Apps_Except_Adobe.sh v1.0
+# List_All_User_Installed_Apps_Except_Adobe.sh v1.3
 #
-# Finds and lists all installed applications on a Mac within the /Users/ directory.
-#       Short script to list all .app bundles on a Mac using Spotlight to search the contents of the /Users/ directory only.
+# Finds and lists all installed applications on a Mac within the /Applications/ and /Users/ directories.
+#       Short script to list all .app bundles on a Mac using Spotlight to search the contents of the /Applications/ and /Users/ directorys.
 #       Apple applications are excluded using the com.apple.* bundle identifier.
 #       Adobe applications are excluded using the com.adobe.* bundle identifier.
 # Greg Knackstedt
