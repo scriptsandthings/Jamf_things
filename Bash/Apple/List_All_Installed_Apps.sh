1070a871f3b2dc5edf72e30e955a2abbd59781e3
Scripts and Things <ShitttyScripts@gmail.com>
2022-06-10 09:07:39 -0400

    Update List_All_Installed_Apps.sh

--- Bash/Apple/List_All_Installed_Apps.sh
+++ Bash/Apple/List_All_Installed_Apps.sh
@@ -1,1 +1,13 @@
-
+#!/bin/zsh
+#######################
+#
+# List_All_Installed_Apps.sh v1.0
+#
+# Finds and lists all installed applications on a Mac.
+#       Short script to list all .app bundles on a Mac using Spotlight to search the entire drive.
+# Greg Knackstedt
+# 6.10.2022
+#
+######################
+#
+mdfind -onlyin /Applications/ '(kMDItemCFBundleIdentifier != "com.apple.*" && kMDItemCFBundleIdentifier != "com.adobe.*") && kMDItemKind == "Application"' | sort -g
