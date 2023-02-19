af637dcaa0d3e68d233bc042e0c64f6a8688255c
Scripts and Things <ShitttyScripts@gmail.com>
2023-02-18 21:00:30 -0500

    Update and rename HardwareType_isLaptop.zsh to HardwareType_isLaptop.sh
    updated with if/else to get proper results for Jamf smart group

--- Extension Attributes/Asset Management/HardwareType_isLaptop.sh
+++ Extension Attributes/Asset Management/HardwareType_isLaptop.sh
@@ -1,8 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
 #
+# Script tested as working in bash + zsh
 ####################################################################
-# HardwareType_isLaptop.zsh
-# v1.0
+# HardwareType_isLaptop
+# v1.1
 #
 # Extension attribute to identify if a system is a laptop or desktop by detecting the presence of a battery.
 # Value will be "Yes" if system is a laptop or "No" if the system is a desktop. 
@@ -17,5 +18,14 @@
 # Check if system has an internal battery
 isLaptop=$(/usr/sbin/ioreg -c AppleSmartBattery -r | awk '/BatteryInstalled/ {print $3}')
 #
-# Set result value
-/bin/echo "<result>$isLaptop</result>"
+# If system is a laptop, set result to Yes
+if [[ $isLaptop == "Yes" ]]; then
+	echo "System is a laptop"
+ 	result="Yes"
+else
+# Else system is a desktop, set result to No
+	echo "System is a desktop"
+	result="No"
+fi
+# Echo the result for collection
+/bin/echo "<result>$result</result>"
